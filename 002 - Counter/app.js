92fde4666495253880a8b015fc3386c938fdfe4f
Abdulkadir Adamu <adamuhammadabdulkadir@gmail.com>
2021-06-07 15:45:28 +0100

    Done!

---
+++ 002 - Counter/app.js
@@ -0,0 +1,15 @@
+const count = document.querySelector('.count');
+const btnAdd = document.querySelectorAll('.btn');
+let counter = 0;
+
+btnAdd.forEach((btn) => {
+    btn.addEventListener('mousedown', () => {
+        if(btn.textContent === '➕') {
+            counter++;
+        } else if(btn.textContent === '➖') {
+            counter--
+        }
+        count.textContent = counter;
+    })
+
+})
